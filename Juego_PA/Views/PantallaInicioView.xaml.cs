45b3ac490e95aaf55af56e68d1acbd81d989ae88
wicho265 <javier26533@gmail.com>
2023-09-27 16:47:27 -0600

    agreger el boton de salir

--- Juego_PA/Views/PantallaInicioView.xaml.cs
+++ Juego_PA/Views/PantallaInicioView.xaml.cs
@@ -24,5 +24,10 @@ namespace Juego_PA.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
     }
 }
